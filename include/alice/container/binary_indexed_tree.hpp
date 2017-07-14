886437f1445e0f9bdf5023ffd620e221acb80081
Entropy Increaser <eirig@qq.com>
2017-07-14 10:33:32 +0800

    basic BIT

--- include/alice/container/binary_indexed_tree.hpp
+++ include/alice/container/binary_indexed_tree.hpp
@@ -17,6 +17,7 @@ template <class T, std::size_t N, class Combination = std::plus<T> >
 class binary_indexed_tree {
 private:
 	T tree[N];
+	Combination comb;
 
 public:
 	typedef T value_type;
@@ -29,18 +30,29 @@ public:
 	{}
 #endif
 
-	binary_indexed_tree(const binary_indexed_tree& copied) {
-		std::copy(tree, tree + N, copied.tree);
-	}
+	binary_indexed_tree(const binary_indexed_tree& copied) { std::copy(tree, tree + N, copied.tree); }
+
+#ifdef ALICE_CPP11
+	binary_indexed_tree(binary_indexed_tree&& rhs)
+	{ std::swap(tree, rhs.tree); }
+#endif
 
 	binary_indexed_tree(const value_type& repeated_value);
 
 	template <class InputIterator>
 	binary_indexed_tree(InputIterator first);
 
-	T query(size_type at);
+	value_type query(size_type at) {
+		value_type ret = tree[at - 1];
+		for (at -= low_bit(at); at; at -= low_bit(at))
+			ret = comb(tree[at - 1], ret);
+		return ret;
+	}
 
-	void change(size_type at);
+	void change(size_type at, const value_type& v) {
+		for (; at <= N; at += low_bit(at))
+			tree[at - 1] = comb(tree[at - 1], v);
+	}
 };
 
 }
