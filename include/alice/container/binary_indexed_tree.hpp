7af85d0acf4910e11dff8e915bba0ce6178ba823
Entropy Increaser <eirig@qq.com>
2017-07-14 11:10:03 +0800

    many initialization

--- include/alice/container/binary_indexed_tree.hpp
+++ include/alice/container/binary_indexed_tree.hpp
@@ -10,8 +10,12 @@
 
 namespace alice {
 
-inline std::size_t low_bit(std::size_t x) 
+inline std::size_t low_bit(std::size_t x)
 { return x & (~x + 1); }
+
+template<class T, class Combination>
+inline T self_combinate(Combination comb, const T& t)
+{ return comb(t, t); }
 
 template <class T, std::size_t N, class Combination = std::plus<T> >
 class binary_indexed_tree {
@@ -23,24 +27,29 @@ public:
 	typedef T value_type;
 	typedef std::size_t size_type;
 
-	binary_indexed_tree()
-#ifdef ALICE_CPP11
-	= default;
-#else
-	{}
-#endif
+	explicit
+	binary_indexed_tree(const Combination& comb = Combination()) : comb(comb) {}
 
-	binary_indexed_tree(const binary_indexed_tree& copied) { std::copy(tree, tree + N, copied.tree); }
+	binary_indexed_tree(const binary_indexed_tree& copied, const Combination& comb = Combination()) : comb(comb) {
+		std::copy(tree, tree + N, copied.tree);
+	}
 
 #ifdef ALICE_CPP11
 	binary_indexed_tree(binary_indexed_tree&& rhs)
 	{ std::swap(tree, rhs.tree); }
 #endif
 
-	binary_indexed_tree(const value_type& repeated_value);
+	binary_indexed_tree(const value_type& repeated_value,
+		const Combination& comb = Combination()) : comb(comb) {
+		for (size_type i = 0; i < N; ++i)
+			if (i & 1)
+				tree[i] = self_combinate(comb, tree[(low_bit(i + 1) >> 1) - 1]);
+			else
+				tree[i] = repeated_value;
+	}
 
 	template <class InputIterator>
-	binary_indexed_tree(InputIterator first);
+	binary_indexed_tree(InputIterator first, const Combination& comb = Combination());
 
 	value_type query(size_type at) {
 		value_type ret = tree[at - 1];
--- include/alice/container/dynamic_binary_indexed_tree.hpp
+++ include/alice/container/dynamic_binary_indexed_tree.hpp
@@ -1,10 +1,20 @@
 #ifndef ALICE_DYNAMIC_BIT_HPP
 #define ALICE_DYNAMIC_BIT_HPP
 
+#include <vector>
+
 namespace alice {
 
 template <class T, class Combination = std::plus<T>, class Container = std::vector<T> >
 class dynamic_binary_indexed_tree {
+private:
+	Container origin, tree;
+	Combination comb;
+
+public:
+	typedef T value_type;
+	typedef Container container_type;
+	typedef std::size_t size_type;
 
 };
 
