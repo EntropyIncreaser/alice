a00c04e30a79af00a26a01bc2cea9bcf0f96709c
EntropyIncreaser <2743134282@qq.com>
2017-10-26 13:36:21 +0800

    monotone stack

---
+++ include/alice/container/monotone_stack.hpp
@@ -0,0 +1,57 @@
+#ifndef ALICE_MONOTONE_STACK_HPP
+#define ALICE_MONOTONE_STACK_HPP
+
+#include <cstddef>
+
+#include <functional>
+#include <stack>
+
+namespace alice {
+
+template <class T, class Compare = std::less<T>,
+          class ContainerT = std::deque<T>,
+          class ContainerInteger = std::deque<std::size_t> >
+class monotone_stack {
+public:
+	typedef T                value_type;
+	typedef const T&         const_reference;
+	typedef ContainerT       value_container_type;
+	typedef ContainerInteger integer_container_type;
+
+private:
+	Compare comp;
+	value_container_type stk;
+	integer_container_type count;
+
+public:
+	monotone_stack(Compare comp = Compare()) : comp(comp) {}
+
+	void push(const value_type& x) {
+		if (stk.empty()) {
+			stk.push_back(x);
+			count.push_back(1);
+		} else {
+			if (comp(stk.back(), x))
+				++count.back();
+			else {
+				stk.push_back(x);
+				count.push_back(1);
+			}
+		}
+	}
+
+	void pop() {
+		if (!--count.back()) {
+			count.pop_back();
+			stk.pop_back();
+		}
+	}
+
+	const_reference min() const {
+		return stk.back();
+	}
+};
+
+}
+
+#endif // ALICE_MONOTONE_STACK_HPP
